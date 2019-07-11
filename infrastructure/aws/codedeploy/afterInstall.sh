fd51abb350b5e0cf0cc6542c44eb6b449c4beb19
Akshata <honrao.a@northeastern.edu>
2019-07-11 17:01:04 -0400

    final change

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-
+sudo su -
 sudo systemctl stop tomcat.service
-
 sudo rm -rf /opt/tomcat/latest/webapps/docs  /opt/tomcat/latest/webapps/examples /opt/tomcat/latest/webapps/host-manager  /opt/tomcat/latest/webapps/manager /opt/tomcat/latest/webapps/ROOT
-
 sudo chown tomcat:tomcat /opt/tomcat/latest/webapps/librarymanagementsystem-0.0.1-SNAPSHOT.war
-
 # cleanup log files
 sudo rm -rf /opt/tomcat/latest/logs/catalina*
 sudo rm -rf /opt/tomcat/latest/logs/*.log
